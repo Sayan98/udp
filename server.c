4813ce6228834959c0120344159b2d5cf22e15c9
Sayan Goswami <goswami.sayan47@gmail.com>
2019-05-25 15:54:17 +0530

    implement server

--- server.c
+++ server.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/socket.h>
+#include <sys/types.h> 
 #include <strings.h>
-#include <arpa/inet.h>  // needed for `struct sockaddr_in`
+#include <arpa/inet.h>      // needed for `struct sockaddr_in`
+#include <netinet/in.h>     // needed for `struct sockaddr_in`
+
 
 #define PORT    8000
 #define MAXLINE 1024
@@ -13,15 +17,17 @@ void error(char *msg) {
     exit(1);
 }
 
-
 int main() {
     int sockfd;
     struct sockaddr_in servaddr, cliaddr;
 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
+    char buffer[MAXLINE]; 
+    char *hello = "Hi, I am the server";
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         // printf("%d\n", sockfd);
         error("Error opening socket.");
+        exit(-1);
     }
 
     bzero(&servaddr, sizeof(servaddr));
@@ -31,6 +37,33 @@ int main() {
     servaddr.sin_family    = AF_INET;   // IPv4
     servaddr.sin_addr.s_addr = INADDR_ANY;
     servaddr.sin_port = htons(PORT);
+
+    if (bind(sockfd, (const struct sockaddr*) &servaddr, sizeof(servaddr)) < 0) {
+        perror("Bind failed");
+        exit(-1);
+    }
+
+    int len, n;
+
+    for(;;) {
+        n = recvfrom(sockfd,
+                     (char *) buffer, 
+                     MAXLINE, 
+                     MSG_WAITALL,
+                     (struct sockaddr*) &cliaddr,
+                     &len);
+
+        buffer[n] = '\0';
+        printf("Client %d sent: %s", cliaddr.sin_addr.s_addr, buffer);
+
+        sendto(sockfd,
+               (const char*) &hello,
+               strlen(hello),
+               MSG_DONTWAIT, 
+               (const struct sockaddr*) &cliaddr, 
+               len);
+        printf("Hello message sent from server.");
+    }
 
     return 0;
 }
